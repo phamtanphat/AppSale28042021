379950758f96387b4fe14830948234d0e7f50b51
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-07-14 20:37:00 +0700

    Setup actionbar

--- app/src/main/java/com/example/appsale28042021/activity/MainActivity.java
+++ app/src/main/java/com/example/appsale28042021/activity/MainActivity.java
@@ -1,17 +1,17 @@
 package com.example.appsale28042021.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.Menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.appsale28042021.R;
 import com.example.appsale28042021.adapter.ProductAdapter;
 import com.example.appsale28042021.model.Product;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,13 +19,18 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView mRcvProduct;
     List<Product> mListProduct;
     ProductAdapter mProductAdapter;
+    Toolbar mToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mRcvProduct = findViewById(R.id.recyclerviewProduct);
+        mToolbar = findViewById(R.id.toolbarProduct);
+        setSupportActionBar(mToolbar);
+
         mListProduct = Product.getMockListProduct();
+
 
         DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
@@ -34,9 +39,12 @@ public class MainActivity extends AppCompatActivity {
 
         mProductAdapter = new ProductAdapter(mListProduct, this, width , height);
 
-
-
         mRcvProduct.setAdapter(mProductAdapter);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
